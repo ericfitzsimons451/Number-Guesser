3c4887d5710d0e29827a05a947ae131e032c06b2
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2018-08-22 11:36:53 -0600

    Refactors code to make functions shorter, despite use of larger codebase.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+/* Variables referencing HTML classes/ids*/
 var userGuess = document.querySelector('.user-guess');
 var guessButton = document.querySelector('.guess-button');
 var clearButton = document.querySelector('.clear-button');
@@ -6,15 +9,17 @@ var numberGuessed = document.querySelector('.number-guessed');
 var bottomReply = document.querySelector('.bottom-reply');
 var inputReset = document.querySelector('.input-reset');
 var randomNumber = Math.ceil(Math.random() * 100);
-console.log(randomNumber);
 
+/* Event Listeners */
 userGuess.addEventListener('keyup', enableButtons);
 guessButton.addEventListener('click', submitGuess);
 clearButton.addEventListener('click', clearInput);
 inputReset.addEventListener('click', resetExecute);
 
+/*FUNctions*/
+
 function enableButtons() {
-  if (userGuess.value === "") {
+  if (userGuess.value === '') {
     guessButton.disabled = true;
     clearButton.disabled = true;
     inputReset.disabled = true;
@@ -22,51 +27,72 @@ function enableButtons() {
     guessButton.disabled = false;
     clearButton.disabled = false;
     inputReset.disabled = false;
-    }
-  };
+  }
+};
+
+function basicState() {
+  randomNumber = Math.ceil(Math.random() * 100);
+  topReply.innerText = 'Guess a number between 1 and 100.';
+  numberGuessed.innerText = 'XX';
+  bottomReply.innerText = 'Believe in yourself.  You can do it!';
+};
 
 function submitGuess(event) {
   event.preventDefault();
   var parsedNumber = parseInt(userGuess.value);
-  console.log(parsedNumber);
   if (parsedNumber <= 0 || parsedNumber >= 101) {
-    alert('Do not do that!');
+    alert('Please guess a number within the specified range.');
   } else {
     compareGuess();
     userGuess.value = '';
   }
 };
 
+function tooHigh() {
+  var parsedNumber = parseInt(userGuess.value);
+  topReply.innerText = 'Your last guess was';
+  numberGuessed.innerText = parsedNumber;
+  bottomReply.innerText = 'That\'s too high.  Guess again.';
+};
+
+function tooLow() {
+  var parsedNumber = parseInt(userGuess.value);
+  topReply.innerText = 'Your last guess was';
+  numberGuessed.innerText = parsedNumber;
+  bottomReply.innerText = 'That\'s too low.  Guess again.';
+};
+
+function notNumber () {
+  topReply.innerText = 'Your last guess was';
+  numberGuessed.innerText = 'Not a Number';
+  bottomReply.innerText = 'Please guess an acutal number.';
+};
+
+function correctGuess () {
+  topReply.innerText = 'Wow!  You got it!';
+  bottomReply.innerText = "Boom goes the dynamite!"
+};
+
 function compareGuess() {
   var parsedNumber = parseInt(userGuess.value);
   if (parsedNumber > randomNumber) {
-    bottomReply.innerText = "That is too high.  Try again.";
-    numberGuessed.innerText = parsedNumber;
-    topReply.innerText = "Your last guess was";
+  tooHigh();
   } else if (parsedNumber < randomNumber) {
-    bottomReply.innerText = "That is too low.  Try again.";
-    numberGuessed.innerText = parsedNumber;
-    topReply.innerText = "Your last guess was";
+    tooLow();
   } else if (isNaN(parsedNumber)) {
-    topReply.innerText = "Your last guess was";
-    bottomReply.innerText = "That's not a number.  Guess again.";
-    numberGuessed.innerText = parsedNumber || "N/A";
+    notNumber();
   } else {
-  topReply.innerText = "Your last guess was";
-  bottomReply.innerText = "Boom goes the dynamite!";
-  };
+    correctGuess();
+  }
 };
 
 function clearInput(event) {
   event.preventDefault();
   userGuess.value = '';
-}
+};
 
 function resetExecute(event) {
   clearInput(event);
-  randomNumber = Math.ceil(Math.random() * 100);
-  topReply.innerText = "Guess a number between 1 and 100!";
-  numberGuessed.innerText = "xx";
-  bottomReply.innerText = "You can do it!"
-
+  basicState();
 };
+
